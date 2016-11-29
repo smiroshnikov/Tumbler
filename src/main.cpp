3d9bb10ed73fbae161240c2d46af3bd2af303f59
smiroshnikov <sergeim.qa@gmail.com>
2016-11-29 14:48:20 +0200

    added cannonball struct and update method for drawing

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,52 @@
 using namespace std; // Have a look here, go to nub tut... stop running
 //using namespace sf;
 
+constexpr int windowWidth{1024}, windowHeight{768};
+constexpr float ballRadius{10.f}, ballVelocity(3.f);
+
+struct Ball {
+    // SFML circle shape is used
+    sf::CircleShape shape;
+
+    // 2D vextor that stores our ball's velocity
+    sf::Vector2f velocity{-ballVelocity, -ballVelocity};
+    // ball constructor
+    // mX , mY starting x and y coordinates
+
+    Ball(float mX, float mY) {
+        shape.setPosition(mX, mY);
+        shape.setRadius(ballRadius);
+        shape.setFillColor(sf::Color::Red);
+        shape.setOutlineThickness(10);
+        shape.setOutlineColor(sf::Color(150, 50, 250));
+        shape.setOrigin(ballRadius, ballRadius);
+    }
+
+    void updateLocation() { shape.move(velocity); }
+};
+
+
 int main(int argc, char **argv) {
 
-    sf::RenderWindow window(sf::VideoMode(1080, 720, 32), "Scuko!");
+    // ball instantiation
+    Ball cannonBall{windowWidth / 3, windowHeight / 3};
+    // Game window
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight, 32), "Scuko!");
+    window.setFramerateLimit(60);
 
+
+    // Game loop
     while (window.isOpen()) { ;
+        window.clear(sf::Color::Black);
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
-        window.clear(sf::Color::Green);
+        window.clear(sf::Color::Black);
+        cannonBall.updateLocation();
+        window.draw(cannonBall.shape);
         window.display();
     }
     return 0;
